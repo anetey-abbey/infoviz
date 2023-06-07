6c645499879a46c2bdf33233d0c7ab69710c18d7
anetey <aneteyabbey@hotmail.com>
2023-06-08 00:08:33 +0200

    test visualization with WDI data

--- build_deploy.sh
+++ build_deploy.sh
@@ -2,3 +2,6 @@
 conda activate jupyterbook
 jupyter-book build .
 ghp-import -n -p -f _build/html
+
+# note that the files generated during the build 
+# need to be pushed to github manually afterwards
