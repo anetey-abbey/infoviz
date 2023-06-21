e4330a6b66956a07a75fe04593512942b58275ca
Pascal <120376246+pascalmathas@users.noreply.github.com>
2023-06-21 18:03:47 +0200

    Book build + online

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,4 +1,6 @@
 # to run this script the first time use sudo chmod +x build_deploy.sh
+# Specific command for Pascal 
+# cd /Users/pascalmathas/Documents/UvA/Kunstmatige_Intelligentie/2022-2023/Informatievisualisatie/GIT/infoviz && conda activate jupyterbook
 conda activate jupyterbook
 jupyter-book build .
 ghp-import -n -p -f _build/html
